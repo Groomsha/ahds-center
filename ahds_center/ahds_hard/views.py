bf64c5d5a5fd54dba6c82826aa58da8eafff73bd
Ihor Cheberiak <Groomsha@users.noreply.github.com>
2024-05-23 16:34:16 +0300

    fix: create html templates hard

--- ahds_center/ahds_hard/views.py
+++ ahds_center/ahds_hard/views.py
@@ -1,8 +1,8 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+#from django.http import HttpResponse
 
 # Create your views here.
 
 
-def index(request) -> HttpResponse:
-    return HttpResponse("AHDS HARD")
+def index(request) -> render:
+    return render(request, 'index.html')
